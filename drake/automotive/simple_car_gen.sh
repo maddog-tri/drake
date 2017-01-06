0d11c82a7152b75590c88c914c7c73b264fe8f26
Matt Marjanovic <maddog@tri.global>
2017-01-06 14:05:09 -0800

    Begin hoisting maliput demo code from drake-maliput into drake proper. (This commit has all the new files, some changes -- and it does not yet compile.)

--- drake/automotive/simple_car_gen.sh
+++ drake/automotive/simple_car_gen.sh
@@ -12,6 +12,8 @@ namespace="drake::automotive"
 source $drake/tools/lcm_vector_gen.sh
 
 gen_lcm_and_vector_from_proto "driving command" $drake/automotive/driving_command_fields.named_vector
+gen_lcm_and_vector "endless road car state" s r heading speed
+gen_lcm_and_vector "endless road oracle output" net_delta_sigma delta_sigma_dot
 gen_lcm_and_vector "euler floating joint state" x y z roll pitch yaw
 gen_vector_proto "idm planner parameters" $drake/automotive/idm_planner_parameters.named_vector
 gen_lcm_and_vector "simple car state" x y heading velocity
